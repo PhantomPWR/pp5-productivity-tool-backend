469f10f7b2bc14b5859278f4d13d98f239c1e800
Jean de Villiers <jeandevilliers@me.com>
2023-06-03 14:19:37 +0000

    Update tasks views.py with permission_classes

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Task
@@ -12,12 +12,31 @@ class TaskList(APIView):
     List all tasks
     No Create view (post method), as task creation handled by django signals
     """
+
+    serializer_class = TaskSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
     def get(self, request):
         tasks = Task.objects.all()
         serializer = TaskSerializer(
             tasks, many=True, context={'request': request}
         )
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TaskSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 class TaskDetail(APIView):
