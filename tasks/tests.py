0924c720a62e6e3c2e1d1f80b79ff3301ec9e484
Jean de Villiers <jeandevilliers@me.com>
2023-06-04 11:57:56 +0000

    Add postlist view tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Task
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TaskListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='rick', password='allen')
+
+    def test_can_list_tasks(self):
+        rick = User.objects.get(username='rick')
+        Task.objects.create(owner=rick, title='a title')
+        response = self.client.get('/tasks/')
+        # self.assertEqual(response.status_code, status.HTTP_201_CREATED)  # Test Fail
+        self.assertEqual(response.status_code, status.HTTP_200_OK)  # Test Pass
+        print(response.data)
+        print(len(response.data))
+
+    def test_logged_in_user_can_create_task(self):
+        self.client.login(username='rick', password='allen')
+        response = self.client.post('/tasks/', {'title': 'a title'})
+        count = Task.objects.count()
+        self.assertEqual(count, 1)
+        # self.assertEqual(response.status_code, status.HTTP_200_OK)  # Test Fail
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)  # Test Pass
+
+    def test_user_not_logged_in_cant_create_task(self):
+        response = self.client.post('/tasks/', {'title': 'a title'})
+        # self.assertEqual(response.status_code, status.HTTP_200_OK)  # Test Fail
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)  # Test Pass
