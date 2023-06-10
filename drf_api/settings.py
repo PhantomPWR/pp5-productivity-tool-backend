be20e731013c1feb83e24f092f348cff6c60e6ea
Jean de Villiers <jeandevilliers@me.com>
2023-06-10 08:52:38 +0000

    Update CLIENT_ORIGIN

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -129,6 +129,11 @@ MIDDLEWARE = [
 #         r"^https://.*\.gitpod\.io$",
 #      ]
 
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+     ]
+
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
