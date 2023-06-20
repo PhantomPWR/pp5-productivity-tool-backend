86de3d1a248f3026fa2ff63672b602837508d87b
Jean de Villiers <jeandevilliers@me.com>
2023-06-20 20:30:59 +0000

    Reset settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -40,7 +40,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DATETIME_FORMAT': '%d %m %Y',
+    'DATETIME_FORMAT': '%d %b %Y',
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
@@ -220,8 +220,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-DATE_INPUT_FORMATS = ["%d-%m-%Y"]
 
 
 # Static files (CSS, JavaScript, Images)
