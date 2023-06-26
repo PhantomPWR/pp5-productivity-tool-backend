9805a71bf1f2e2aafbb45f3143ef956ab8f26ef2
Jean de Villiers <jeandevilliers@me.com>
2023-06-26 06:51:04 +0000

    Update assign_to in models.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -189,6 +189,7 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
+    print('connected')
 
 
 # Password validation
--- tasks/models.py
+++ tasks/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from datetime import date
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
 from categories.models import Category
 
 
