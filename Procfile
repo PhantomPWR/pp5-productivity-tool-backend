b0c0ac577e0615d837ab7bd3d206e6adcbf1e2bc
Jean de Villiers <jeandevilliers@me.com>
2025-03-11 17:23:00 +0000

    Configure static files for Heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,5 @@
+
+release: python manage.py migrate
+release: python manage.py collectstatic --noinput
 release: python manage.py makemigrations && python manage.py migrate
  web: gunicorn drf_api.wsgi
--- drf_api/settings.py
+++ drf_api/settings.py
@@ -124,6 +124,7 @@ SITE_ID = 1
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -256,6 +257,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Enable WhiteNoise to serve static files efficiently
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
