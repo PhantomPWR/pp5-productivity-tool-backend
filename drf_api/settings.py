3751f823ac65b18a18e904d06ed8f4b4ef257aa2
Jean de Villiers <jeandevilliers@me.com>
2025-03-12 13:11:29 +0000

    Update handling client-origin-dev

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -151,11 +151,19 @@ if 'CLIENT_ORIGIN' in os.environ:
         os.environ.get('CLIENT_ORIGIN')
     ]
 
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-    ]
+# if 'CLIENT_ORIGIN_DEV' in os.environ:
+#     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+#     CORS_ALLOWED_ORIGIN_REGEXES = [
+#         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+#     ]
+
+client_origin_dev = os.environ.get('CLIENT_ORIGIN_DEV', '')
+match = re.match(r'^.+-', client_origin_dev, re.IGNORECASE)
+
+if match:
+    extracted_url = match.group(0)
+else:
+    extracted_url = None  # or handle the error accordingly
 
 CORS_ALLOW_CREDENTIALS = True
 
