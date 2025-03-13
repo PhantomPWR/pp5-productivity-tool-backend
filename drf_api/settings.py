3e60cb8d15948464ef02a9490ee85fc7e2303a7e
Jean de Villiers <jeandevilliers@me.com>
2025-03-13 17:17:12 +0000

    Setting.py line 62-63

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -59,8 +59,8 @@ JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH = {
     'USE_JWT': True,
-    # 'JWT_AUTH_COOKIE:': 'drf-api-auth',
-    # 'JWT_AUTH_REFRESH_COOKIE': 'drf-api-refresh-token',
+    'JWT_AUTH_COOKIE:': 'drf-api-auth',
+    'JWT_AUTH_REFRESH_COOKIE': 'drf-api-refresh-token',
 }
 
 REST_AUTH_SERIALIZERS = {
