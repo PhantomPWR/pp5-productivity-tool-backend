d75b3936a8faf59c3a56c02ef5c89e5246f5b999
Jean de Villiers <jeandevilliers@me.com>
2025-03-15 16:27:38 +0000

    Back to square 1

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,7 +62,7 @@ if 'DEV' not in os.environ:
     ]
 
 # REST_USE_JWT = True
-USE_JWT = True
+# USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'drf-api-auth'
 JWT_AUTH_REFRESH_COOKIE = 'drf-api-refresh-token'
